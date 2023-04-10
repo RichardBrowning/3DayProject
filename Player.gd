6445ba729d94c8890cd18ad467385ae91d942b11
Richard Browning <mi888uzl@outlook.com>
2023-04-10 20:01:22 +0800

    first commit

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody
+
+const SPEED = 3.0;
+const JUMP_VELOCITY = 4.5;
+const GRAVITY = -.2;
+
+var velocity = Vector3.ZERO;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += GRAVITY;
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		velocity.y = JUMP_VELOCITY;
+	
+	var input_dir := Input.get_vector( "ui_left", "ui_right", "ui_up", "ui_down");
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized();
+	if direction:
+		velocity.x = direction.x * SPEED;
+		velocity.z = direction.z * SPEED;
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED);
+		velocity.z = move_toward(velocity.z, 0, SPEED);
+		
+	
+	move_and_slide(velocity, Vector3.UP);
