226c55a7533a48870b1759c31a61707f340210ea
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-04-13 16:20:02 +0800

    Move and slide is successful

--- Enemy.gd
+++ Enemy.gd
@@ -1,18 +1,25 @@
 extends KinematicBody
 
 const GRAVITY = -.2;
-var velocity = Vector3.ZERO
+const MOVE_SPEED = 3.5;
+onready var target_position = get_parent().get_node("CollisionArea").transform.origin;
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var isMoving = true;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
-	move_and_slide();
+func _process(delta):
+	if isMoving:
+		var dir = (target_position - translation).normalized()
+		move_and_slide(dir);
+	#print(target_position)
+
+# func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
