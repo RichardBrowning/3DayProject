8f8c1b5943a1b03671ca2134552c06629c16fcd7
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-04-13 16:38:14 +0800

    backup everything

--- Enemy.gd
+++ Enemy.gd
@@ -3,21 +3,30 @@ extends KinematicBody
 const GRAVITY = -.2;
 const MOVE_SPEED = 3.5;
 onready var target_position = get_parent().get_node("CollisionArea").transform.origin;
+onready var collision_area = get_parent().get_node("CollisionArea");
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var isMoving = true;
+var isCaught = false;
+var trippedOver = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#connect("body_entered", self, "_on_Area_body_entered");
 	pass # Replace with function body.
 
 func _process(delta):
 	if isMoving:
 		var dir = (target_position - translation).normalized()
 		move_and_slide(dir);
-	#print(target_position)
+	if isCaught || trippedOver:
+		isMoving = false;
+		
+		
+#func _on_Area_body_entered(body: Node) -> void:
+#	print("Collided");
 
 # func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Level.gd
+++ Level.gd
@@ -11,7 +11,8 @@ onready var dest = null;
 func _ready():
 	pass # Replace with function body.
 
-
+func _process(delta):
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
