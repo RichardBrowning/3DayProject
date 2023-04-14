088a7137f4d1bc27938db9969cb63050c63fdc9d
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-04-14 11:44:36 +0800

    stuck

--- Level.gd
+++ Level.gd
@@ -5,14 +5,17 @@ onready var dest = null;
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var dooropen_animation = get_node("elevator/AnimationPlayer")
+dooropen_animation.set_loop(true)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	pass
+	dooropen_animation.play("Door0Opens");
+	dooropen_animation.play("Door1Opens");
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
